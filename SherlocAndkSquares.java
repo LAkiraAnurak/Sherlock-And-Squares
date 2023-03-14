18ccbcec3044ee90bd414d4c21e5e4fd50ddb266
Mahendra Krishnamachari Kakinada <127008524+LAkiraAnurak@users.noreply.github.com>
2023-03-14 11:43:49 +0530

    View Code

---
+++ SherlocAndkSquares.java
@@ -0,0 +1,60 @@
+package pack1;
+
+import java.io.*;
+import java.util.stream.*;
+
+class SASResult {
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b) 
+    {
+    // Write your code here
+        int c=0;
+        int x=(int)Math.sqrt(a),y=(int)Math.sqrt(b);
+        for(int i=x;i<=y;i++)
+        {
+            x=i*i;
+            if(!(x<a)) c++;
+            //if(x>=b) break;
+        }
+        return c;
+    }
+
+}
+
+public class SherlocAndkSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int a = Integer.parseInt(firstMultipleInput[0]);
+
+                int b = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = SASResult.squares(a, b);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
